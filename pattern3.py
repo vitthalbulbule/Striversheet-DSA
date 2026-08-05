ca19b0ddcc7d5622ac2fb108e8363ce3c132b6ea
vitthalbulbule <vbulbule205@gmail.com>
2026-08-05 15:51:00 +0530

    pattern3

---
+++ pattern3.py
@@ -0,0 +1,14 @@
+# 1
+# 12
+# 123
+# 1234
+# 12345
+
+
+for i in range(1,6):
+    for j in range (1,6):
+        if j<=i:
+            print(i,end='')
+        else:
+            print(end='')
+    print()
